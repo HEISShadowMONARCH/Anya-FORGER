1459079922d930eb2e0392318d1dabec9354b76a
HEISShadowMONARCH <136151916+HEISShadowMONARCH@users.noreply.github.com>
2023-08-12 09:42:22 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,7 +9,8 @@ import axios from 'axios'
 global.owner = [
   ['917605902011', 'GURU', true],
   ['6281255369012', 'Ash', true], 
-  [''] 
+  ['2348123481780' 'Shadow' true],
+  ['']
 ] //Number of owners
 
 global.mods = ['917605902011','32460248586'] 
@@ -47,10 +48,10 @@ global.APIKeys = { // APIKey Here
 }
 
 // Sticker WM
-global.botname = 'ᴛʜᴇ ɢᴜʀᴜ-ʙᴏᴛ'
+global.botname = 'ꪖꪀꪗꪖ-ᠻꪮ𝕣ᧁꫀ𝕣'
 global.premium = 'true'
-global.packname = 'GURU┃ᴮᴼᵀ' 
-global.author = '@Asliguru' 
+global.packname = '𓁹‿𓁹' 
+global.author = '@shadow' 
 global.igfg = '▢ Follow on Instagram\nhttps://www.instagram.com/asli_guru69\n' 
 global.dygp = 'https://chat.whatsapp.com/BibVE6tUICp3qOm2a1Ur8f'
 global.fgsc = 'https://github.com/Guru322/GURU-BOT-V1' 
