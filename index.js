78178aada3e6a35e30b5a52ee018d559384d9540
HEISShadowMONARCH <136151916+HEISShadowMONARCH@users.noreply.github.com>
2023-08-12 09:45:06 +0100

    Update index.js

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ const { name, author } = require(join(__dirname, './package.json')) // https://w
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('GURU -  BOT', {
+say('ꪖꪀꪗꪖ-ᠻꪮ𝕣ᧁꫀ𝕣', {
   font: 'chrome',
   align: 'center',
   gradient: ['red', 'magenta']
